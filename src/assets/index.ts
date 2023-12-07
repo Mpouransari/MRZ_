f2ef321c59840d89b6da8a74d408f361f80c9c19
Tarantula Trade <85387547+Mpouransari@users.noreply.github.com>
2023-12-08 03:19:51 +0330

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import { Data } from "../interfaces";
+
+export const data: Data[] = [
+    {id: 1,content: 'آمد',status: 'آ'},{id: 2,content: 'آمد',status: 'آ'},
+   
+    {id: 6,content: 'بد',status: 'ب'},{id: 7,content: 'Flash',status: 'ب'},
+
+    {id: 11,content: 'Green Lantern',status: 'س'},{id: 12,content: 'Green Lantern',status: 'س'},
+
+    {id: 16,content: 'Batman',status: 'د'},{id: 17,content: 'Batman',status: 'د'},
+
+    {id: 21,content: 'Batman',status: 'ت'},{id: 22,content: 'Batman',status: 'ت'},
+]
