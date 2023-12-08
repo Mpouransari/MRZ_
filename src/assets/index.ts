001b235dbae236748eefcaa2097edb0466860b5e
Tarantula Trade <85387547+Mpouransari@users.noreply.github.com>
2023-12-08 14:27:04 +0330

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,46 @@
 import { Data } from "../interfaces";
 
 export const data: Data[] = [
-    {id: 1,content: 'آمد',status: 'آ'},{id: 2,content: 'اسب',status: 'آ'},{id: 3,content: 'آتوسا',status: 'آ'},{id: 4,content: 'اسد',status: 'آ'},
+    {id: 1,content: 'آمَد',status: 'آ'},{id: 2,content: 'اَسب',status: 'آ'},{id: 3,content: 'آتوسا',status: 'آ'},{id: 4,content: 'اَسَد',status: 'آ'},
    
     {id: 5,content: 'بابا',status: 'ب'},{id: 6,content: 'بادام',status: 'ب'},
 
-    {id: 7,content: 'سام',status: 'س'},{id: 8,content: 'سباn',status: 'س'},
+    {id: 7,content: 'سام',status: 'س'},{id: 8,content: 'سَرد',status: 'س'},
 
-    {id: 9,content: 'داس',status: 'د'},{id: 10,content: 'درد',status: 'د'},
+    {id: 9,content: 'داس',status: 'د'},{id: 10,content: 'دَرد',status: 'د'},
 
     {id: 11,content: 'توت',status: 'ت'},{id: 12,content: 'تاب',status: 'ت'},
+
+    {id: 13,content: 'آب',status: 'آ'},{id: 14,content: 'داد',status: 'د'},
+
+    {id: 15,content: 'بود',status: 'ب'},{id: 16,content: 'داماد',status: 'د'},
+
+    {id: 17,content: 'بَست',status: 'ب'},{id: 18,content: 'دارَد',status: 'د'},
+
+    {id: 19,content: 'اَست',status: 'آ'},{id: 20,content: 'دادَم',status: 'د'},
+
+    {id: 21,content: 'آمَدَم',status: 'آ'},{id: 22,content: 'بودَم',status: 'ب'},
+
+    {id: 23,content: 'بَستَم',status: 'ب'},{id: 24,content: 'دارَم',status: 'د'},
+
+    {id: 25,content: 'آراد',status: 'آ'},{id: 26,content: 'سَردار',status: 'س'},
+
+    {id: 27,content: 'اَسما',status: 'آ'},{id: 28,content: 'مادَر',status: 'م'},
+
+    {id: 29,content: 'مَرد',status: 'م'},{id: 30,content: 'بَرادَرَم',status: 'ب'},
+
+    {id: 31,content: 'تَبَر',status: 'ت'},{id: 32,content: 'آدامس',status: 'آ'},
+
+    {id: 33,content: 'مَسموم',status: 'م'},{id: 34,content: 'سَبَد',status: 'س'},
+
+    {id: 35,content: 'آرام',status: 'آ'},{id: 36,content: 'سَما',status: 'س'},
+
+    {id: 37,content: 'تَرس',status: 'ت'},{id: 38,content: 'بَبر',status: 'ب'},
+
+    {id: 39,content: 'سَردَرد',status: 'س'},{id: 40,content: 'اَبرو',status: 'آ'},
+
+    {id: 41,content: 'آدَم',status: 'آ'},{id: 42,content: 'مو',status: 'م'},
+
+
+
 ]
