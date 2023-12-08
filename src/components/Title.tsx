0905e8e1f76093fe31e073db6ec82baa9f951d4b
Tarantula Trade <85387547+Mpouransari@users.noreply.github.com>
2023-12-08 21:55:01 +0330

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,9 +3,9 @@ export const Title = () => {
     return (
         <><div className="title flex">
             <h1>
-            <img src="/image/0.png" alt="persian Alphabet" width="100" height="100"></img>
+            <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/0.png" alt="persian Alphabet" width="100" height="100"></img>
             ساخت جمله با کلمات
-            <img src="/image/0.png" alt="persian Alphabet" width="100" height="100"></img>
+            <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/0.png" alt="persian Alphabet" width="100" height="100"></img>
                 
             </h1>
             
@@ -25,7 +25,7 @@ export const Title = () => {
                 <br></br>
                 <br></br>
 
-                    <img src="/image/MP.png" alt="persian Alphabet" width="100" height="100"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/MP.png" alt="persian Alphabet" width="100" height="100"></img>
                 </div>
         </div>
         <br></br>
@@ -41,25 +41,25 @@ export const Title = () => {
 
             <div className="row_img">
                 <div className="img2">
-                    <img src="/image/6.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/6.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/5.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/5.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/7.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/7.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/4.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/4.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/3.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/3.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/2.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/2.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
                 <div className="img2">
-                    <img src="/image/1.png" alt="persian Alphabet" width="150" height="150"></img>
+                    <img src="https://github.com/Mpouransari/MRZ_/blob/main/image/1.png" alt="persian Alphabet" width="150" height="150"></img>
                 </div>
             </div>
             </>
