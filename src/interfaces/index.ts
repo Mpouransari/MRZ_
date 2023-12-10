1c5bfbf2ad749bb0d7c36be7efd9b50748358814
Tarantula Trade <85387547+Mpouransari@users.noreply.github.com>
2023-12-10 22:39:51 +0330

    Update index.ts
    Add Category (ن)

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
-export type Status = 'آ' | 'ب' | 'س'| 'د'| 'ت' |'۱'| '۲'|'۳'|'۴'|'۵'|'۶'|'ر'|'م'
+export type Status = 'آ' | 'ب' | 'س'| 'د'| 'ت' |'۱'| '۲'|'۳'|'۴'|'۵'|'۶'|'ر'|'م'|'ن'
 
 export interface Data {
     id: number
